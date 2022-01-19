fd305e5592352261fb648c41c6e338a5eaf85c82
Claire Marshall <marshallc@newberry.org>
2022-01-19 15:16:41 -0600

    Clarified variable names

--- bibid_workflow.py
+++ bibid_workflow.py
@@ -13,22 +13,22 @@ if __name__ == '__main__':
 	# directory = 'image_repository/' ## Test
 	for folder in os.listdir(directory):
 		# pp(folder)
-		f = os.path.join(directory, folder)
-		if os.path.isdir(f) == True:
+		folder_path = os.path.join(directory, folder)
+		if os.path.isdir(folder_path) == True:
 			if 'bib' in folder:
 				new_folder = folder.replace('bib', 'ur')
 				# pp(new_folder)
-				first_file = os.listdir(f)[0]
+				first_file = os.listdir(folder_path)[0]
 				# pp(first_file)
 				bibid = first_file.split('_')[0]
 				# pp(bibid)
-				for file in os.listdir(f):
+				for file in os.listdir(folder_path):
 					# pp(file)
 					if bibid not in file:
 						# pp(file)
 						new_file = bibid + '_' + file 
 						# pp(new_file)
-						os.rename(os.path.join(f, file), os.path.join(f, new_file))
+						os.rename(os.path.join(folder_path, file), os.path.join(folder_path, new_file))
 
 				os.rename(os.path.join(directory, folder), os.path.join(directory, new_folder))
 
